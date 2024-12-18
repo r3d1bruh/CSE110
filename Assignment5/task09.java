0dc8b30733522745990c343bee6df729a7f98b63
Ridoanul Islam <cruxn@outlook.com>
2024-12-18 18:50:25 +0600

    9 done

--- Assignment5/task09.java
+++ Assignment5/task09.java
@@ -1,24 +1,46 @@
 import java.util.Scanner;
-public class template
+public class task09
 {
-    public static void main(String[] nigger)
+    public static void main(String[] Gaben)
     {
         Scanner x = new Scanner(System.in);
+		System.out.println("Plwase enter your passwowd Senpai UwU: ");
 		String passcode = x.nextLine();
-		char[] requirement = passcode.toCharArray();
-		
-        System.out.print(" ");
-        x.close();
-		for(int i =1; i<0;i++)
+		boolean capCheckStatus = false, smallCheckStatus = false, digitCheckStatus = false, spCharStatus = false, passLengthStatus = false;
+		for(int i = 0;i<=passcode.length()-1;i++)
 		{
-			if(){
-				
+			char ram = passcode.charAt(i);
+			if(ram>='a' && ram<='z')
+			{
+				smallCheckStatus = true;
 			}
-			else(){
-				
+			if(ram>='A' && ram<='Z')
+			{
+				capCheckStatus = true;
+			}
+			if(ram>='0' && ram<='9')
+			{
+				digitCheckStatus = true;
+			}
+			if ((ram >= 33 && ram <= 47) || (ram >= 58 && ram <= 64) || (ram >= 91 && ram <= 96) || (ram >= 123 && ram <= 126)) 
+			{ 
+				spCharStatus = true;
 			}
 		}
-
+		if(passcode.length()>=8)
+		{
+			passLengthStatus = true;
+		}
+		System.out.println("Password Strength Status: ");
+		if(capCheckStatus&&smallCheckStatus&&digitCheckStatus&&spCharStatus&&passLengthStatus)
+		{
+			System.out.print("Hard");
+		}
+		else
+		{
+			System.out.print("Soft");
+		}
+		System.out.println("\n\n");
     }
 }    
     
