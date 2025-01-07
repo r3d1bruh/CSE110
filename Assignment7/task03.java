21c329815757b33461ab19ce2d0656819cda301e
Ridoanul Islam <81561750+r3d1bruh@users.noreply.github.com>
2025-01-08 00:22:24 +0600

    Create task03.java

---
+++ Assignment7/task03.java
@@ -0,0 +1,30 @@
+ public class task03
+{
+    public static boolean isTriangle(int alpha, int beta, int gamma)
+    {
+       if((alpha+beta)>gamma && (beta+gamma)>alpha && (alpha+gamma)>beta)
+       {
+        return true;
+       }       
+       else 
+       {
+        return false;
+       }   
+    }
+    public static void triArea(int alpha, int beta, int gamma)
+    {
+       if(isTriangle(alpha,beta,gamma))
+       {
+        double s = (alpha+beta+gamma)/2;
+        System.out.println(Math.sqrt(s*(s-alpha)*(s-beta)*(s-gamma)));
+       }
+       else
+       {
+        System.out.println("Can't form triangle");
+       }
+    }
+    public static void main(String[] args)
+    {
+        triArea(3,2,1);
+    }
+}
