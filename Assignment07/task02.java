053e759f587319bf2e8e1b5a4d40d51b3e45ff9d
Ridoanul Islam <81561750+r3d1bruh@users.noreply.github.com>
2025-01-07 23:17:55 +0600

    Update and rename task02 to task02.java

--- Assignment07/task02.java
+++ Assignment07/task02.java
@@ -5,25 +5,28 @@ public class task02
        double circleArea = Math.PI*Math.pow(Gaben,2);  
        return circleArea;  
     }
-    public static double sphereVolume(double Gaben)
+    public static double sphereVolume(double value)
     {
-       double sphereVolume = (4*Math.PI*Math.pow(Gaben,3))/3;  
+       double sphereVolume = (4*Math.PI*Math.pow(value,3))/3;  
        return sphereVolume;  
     }
     public static void findSpace(double Gaben, String type)
     {
         if(type.equals("circle"))
         {
-            System.out.println(circleArea(Gaben));
+            System.out.println(circleArea(Gaben/2));
         }
         else if(type.equals("sphere"))
         {
-            System.out.println(sphereVolume(Gaben));
+            System.out.println(sphereVolume(Gaben/2));
         }
-        
+        else
+        {
+            System.out.println("Wrong parameter");
+        }    
     }
     public static void main(String[] args)
     {
-        findSpace(10,"circle");
+        findSpace(4,"sphere");
     }
 }
