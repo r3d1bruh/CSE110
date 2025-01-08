6bd0748f8d960c886d9ea5963a49b443b4c80975
Ridoanul Islam <81561750+r3d1bruh@users.noreply.github.com>
2025-01-08 16:48:21 +0600

    Update task04.java

--- Assignment7/task04.java
+++ Assignment7/task04.java
@@ -1,4 +1,4 @@
- public class task04
+ public class Main
 {
     public static boolean isPrime(int suspect)
     {
@@ -13,7 +13,7 @@
     }
     public static boolean isPerfect(int suspect)
     {
-        int sum =0;
+       int sum =0;
        for (int StatTrak = 1; StatTrak < suspect; StatTrak++) 
        {
         if (suspect % StatTrak == 0) 
@@ -28,8 +28,25 @@
        }
        return false;
     }
+    public static int special_sum(int suspect)
+    {
+       int sum = 0;
+       for (int StatTrak = 0; StatTrak <= suspect; StatTrak++) 
+       {
+            if(isPrime(StatTrak) && StatTrak!=1)
+            {
+                sum+=StatTrak;
+            }
+            if(isPerfect(StatTrak))
+            {
+                sum+=StatTrak;
+            }
+       } 
+       return sum;
+    }
     public static void main(String[] args)
     {
-     System.out.println(isPerfect(33));
+      int result = special_sum(8);
+      System.out.println(result);
     }
 }
